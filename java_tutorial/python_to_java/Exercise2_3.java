c945d6328ce39507782c9dbfa08f50b71aa0ebe7
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-12 21:05:54 +0300

    Complete Exercise2_3 - 10-digit number.

---
+++ java_tutorial/python_to_java/Exercise2_3.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+/**
+ * A program that asks the user for a 10-digit number
+ * and will then print it in two lines.
+ *
+ * The first line will contain the numbers in the odd positions
+ * and the second the numbers in the even positions.
+ *
+ * Each number must be in a column by itself.
+ */
+public class Exercise2_3 {
+  public static void main(String[] argss) {
+    // Get a 10 digit number from the user.
+    String num = getTenDigitNumber();
+    // Print number in odd positions.
+    for (int odd = 0; odd <= 8; odd += 2) {
+      System.out.print(Character.toString(num.charAt(odd)) + " ");
+    }
+    // Change line.
+    System.out.println();
+    // Print number in even positions.
+    for (int odd = 1; odd <= 9; odd += 2) {
+      System.out.print(" " + Character.toString(num.charAt(odd)));
+    }
+    System.out.println();
+  }
+
+  /**
+   * Prompts user for a 10 digit number.
+   */
+  public static String getTenDigitNumber() {
+      // Declare and initialize a simple text scanner.
+      Scanner scanner = new Scanner(System.in);
+      String byteString;
+      do {
+          System.out.print("Enter 10 digit number: ");
+          byteString = scanner.nextLine();
+      } while (! (byteString.matches("[0-9]+") && byteString.length() == 10));
+      // Close the scanner.
+      scanner.close();
+      return byteString;
+  }
+}
