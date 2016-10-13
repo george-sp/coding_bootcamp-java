58db2dde1c0163ac096599be78fc4dfd75a74bd9
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-13 12:57:56 +0300

    Start Exercise1: Create class Circle - (step1).

---
+++ java_tutorial/classes/Circle.java
@@ -0,0 +1,15 @@
+/**
+ * Exercise1:
+ * https://codeandwork.github.io/courses/java/classes-p.html#/38
+ */
+/*
+ * A class that represents a circle shape
+ */
+public class Circle {
+    // the x coordinate
+    private int x;
+    // the y coordinate
+    private int y;
+    // the radius
+    private int r;
+}
