829bb90c46821e293fe0eaa22c89c360d6fc6d11
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-13 17:01:46 +0300

    Create the Author class.

---
+++ java_tutorial/classes/Exercise3/Author.java
@@ -0,0 +1,18 @@
+public class Author {
+
+    private String name;
+
+    public Author(){}
+
+    public Author(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
