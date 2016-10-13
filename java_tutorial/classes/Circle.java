2be4451941ab9dc8d52cf52afde2b5ea0ead88f0
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-13 13:03:25 +0300

    Create setters & getters - (step2).

--- java_tutorial/classes/Circle.java
+++ java_tutorial/classes/Circle.java
@@ -12,4 +12,32 @@ public class Circle {
     private int y;
     // the radius
     private int r;
+
+    /*
+     * Setters & Getters
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setR(int r) {
+        this.r = r;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public int getR() {
+        return this.r;
+    }
+
 }
