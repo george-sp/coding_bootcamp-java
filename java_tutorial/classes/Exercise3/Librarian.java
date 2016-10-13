732f69c007b50a306dc9b1bca0ba99ee979c0403
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-13 23:43:45 +0300

    Hotfix. Fill the empty Librarian class.

--- java_tutorial/classes/Exercise3/Librarian.java
+++ java_tutorial/classes/Exercise3/Librarian.java
@@ -1,4 +1,51 @@
-
 public class Librarian {
 
+	// the library that he manages
+	private Library library;
+	
+	/**
+	 * Constructors
+	 */
+	// Default Constructor
+	public Librarian(){}
+	
+	public Librarian(Library library) {
+		this.library = library;
+	}
+	
+	/**
+	 * Setters & Getters
+	 */
+	public void setLibrary(Library library) {
+		this.library = library;
+	}
+	
+	public Library getLibrary() {
+		return this.library;
+	}
+	
+	/**
+	 * Receives an author name and delegates the request 
+	 * to the library's printBookFromAuthor method.
+	 * @param authorName
+	 */
+	public void findMeBooksFromAuthor(String authorName) {
+		this.library.printBookFromAuthor(authorName);
+	}
+	
+	/**
+	 * Delegates the request to the library's printAvailableBooks method.
+	 */
+	public void findMeAvailableBooks() {
+		this.library.printAvailableBooks();
+	}
+	
+	/**
+	 * Receives a book's title and delegates the request
+	 * to the library's printBookDetails method.
+	 * @param bookTitle
+	 */
+	public void findMeBook(String bookTitle) {
+		this.library.printBookDetails(bookTitle);
+	}
 }
