f0c9067ca2467d33b593fd943d47df120bb2050c
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-13 22:43:03 +0300

    Create the Book class.

--- java_tutorial/classes/Exercise3/Author.java
+++ java_tutorial/classes/Exercise3/Author.java
@@ -1,13 +1,21 @@
 public class Author {
 
+    // the name of the author.
     private String name;
 
+    /*
+     * Constructors
+     */
+    // Default Constructor
     public Author(){}
 
     public Author(String name) {
         this.name = name;
     }
 
+    /*
+     * Setters & Getters
+     */
     public void setName(String name) {
         this.name = name;
     }
---
+++ java_tutorial/classes/Exercise3/Book.java
@@ -0,0 +1,124 @@
+public class Book {
+
+    // book's title
+    private String title;
+    // book's author
+    private Author author;
+    // book's isbn: should not be changed after the initialization
+    private final String ISBN;
+    // physcial copies of the book
+    private int physicalCopies;
+    // available copies of the book
+    private int availableCopies;
+    // times the book was rented
+    private int timesRented;
+
+    /**
+     * Constructors
+     */
+    // Default Constructors
+    public Book() {
+        this.ISBN = "0991221344";
+    }
+
+    // Full argument Constructor
+    public Book(String title, Author author, String isbn, int physicalCopies,
+                int availableCopies, int timesRented) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = isbn;
+        this.physicalCopies = physicalCopies;
+        this.availableCopies = availableCopies;
+        this.timesRented = timesRented;
+    }
+
+    /**
+     * Setters & Getters
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public void setPhysicalCopies(int physicalCopies) {
+        this.physicalCopies = physicalCopies;
+    }
+
+    public void setAvailableCopies(int availableCopies) {
+        this.availableCopies = availableCopies;
+    }
+
+    public void setTimesRented(int timesRented) {
+        this.timesRented = timesRented;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public Author getAuthor() {
+        return this.author;
+    }
+
+    public String getISBN() {
+        return this.ISBN;
+    }
+
+    public int getPhysicalCopies() {
+        return this.physicalCopies;
+    }
+
+    public int getAvailableCopies() {
+        return this.availableCopies;
+    }
+
+    public int getTimesRented() {
+        return this.timesRented;
+    }
+
+    /**
+     * Returns the details of the book in printable way.
+     */
+    public String toString() {
+        return "Title: " + getTitle() +
+               "Author: " + getAuthor().getName() +
+               "ISBN: " + getISBN() +
+               "Physical copies: " + getPhysicalCopies() +
+               "Available copies: " + getAvailableCopies() +
+               "Times rented: " + getTimesRented();
+    }
+
+    /**
+     * Checks if there is an available copy for renting.
+     * If yes, then it prints a message of success.
+     */
+    public void rentPhysicalCopy() {
+        if (isAvailable()) {
+            this.availableCopies--;
+            System.out.println("Book rented!");
+        }
+    }
+
+    /**
+     * Returns true when there is at least one available copy of the book, otherwise false.
+     */
+    public boolean isAvailable() {
+        if (this.availableCopies >= 1) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Returns true or false if the given name is the name of the book's author.
+     */
+    public boolean hasAuthor(String name) {
+        if (this.author.getName().equals(name)) {
+            return true;
+        }
+        return false;
+    }
+}
