578a47ef163c395fad202ad1dc73858ed7f15072
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-14 22:49:33 +0300

    Create the TicTacToeGame class.

--- java_tutorial/objects/TicTacToe/TicTacToe.java
+++ java_tutorial/objects/TicTacToe/TicTacToe.java
@@ -1,7 +1,20 @@
 import java.util.Scanner;
 
+/**
+ * Exercise5 - https://codeandwork.github.io/courses/java/objects.html
+ * Implement a tic tac toe (triliza) game which is played by two players.
+ */
 public class TicTacToe {
     public static void main(String[] args) {
         System.out.println("Welcome to TicTacToe!");
+    }
+
+    private class TicTacToeGame {
+        // Use a two dimensions array to store the results ('X', 'O').
+        private char[][] board;
+
+        public TicTacToeGame() {
+            this.board = new char[3][3];
+        }
     }
 }
