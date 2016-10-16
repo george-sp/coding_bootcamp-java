880e0c864f2486e99de95bb0315853b9a892dd33
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-16 15:46:21 +0300

    Use the Tester class for testing.

---
+++ java_tutorial/inheritance/homework/Tester.java
@@ -0,0 +1,15 @@
+/**
+ * This class is used for testing.
+ */
+public class Tester {
+    public static void main(String[] args) {
+        System.out.println("This is a tester class!");
+
+        Customer c1 = new Customer();
+        // System.out.println("c1) code: " + c1.getCode()
+        //                    + ", name: " + c1.getName()
+        //                    + ", birtyYear:" + c1.getBirthYear()
+        //                    + ", sex:" + c1.getSex());
+        // System.out.println(c1.toString());
+    }
+}
