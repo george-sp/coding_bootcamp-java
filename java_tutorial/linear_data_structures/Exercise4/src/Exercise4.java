4639d1ebb52a6b7c8be13e6a7139ee040fc4d77b
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-20 00:35:30 +0300

    Start Exercise4 - palindrome.

---
+++ java_tutorial/linear_data_structures/Exercise4/src/Exercise4.java
@@ -0,0 +1,13 @@
+
+/**
+ * A program that checks if a given word is a palindrome.
+ * Palindrome words are the words that can be read backwards. (example: madam)
+ * @author george
+ *
+ */
+public class Exercise4 {
+	
+	public static void main(String[] args) {
+		
+	}
+}
