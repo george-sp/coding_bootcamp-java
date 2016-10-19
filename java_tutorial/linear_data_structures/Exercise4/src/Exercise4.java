c8beb27ac2e876452a7e7e14ccd0fc508b7f575b
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-20 00:37:41 +0300

    Complete Exercise4.

--- java_tutorial/linear_data_structures/Exercise4/src/Exercise4.java
+++ java_tutorial/linear_data_structures/Exercise4/src/Exercise4.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 /**
  * A program that checks if a given word is a palindrome.
@@ -8,6 +9,18 @@
 public class Exercise4 {
 	
 	public static void main(String[] args) {
-		
+		System.out.print("Give a word: ");
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		char[] charArray = input.toCharArray();
+		int halfSize = charArray.length / 2;
+		int size = charArray.length;
+		for (int i = 0; i <= halfSize; i++) {
+			if (charArray[i] != charArray[size-1-i]) {
+				System.out.println("Not a palindrome");
+				return;
+			}
+		}
+		System.out.println("Yeah!! You gave a palindrome!");
 	}
 }
