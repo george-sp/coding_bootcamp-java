314d65313d28d49bc01bbadf787f4e85b6891fc6
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-19 11:25:42 +0300

    Create the Transaction class to store info on a book operation.

---
+++ java_tutorial/linear_data_structures/Exercise3(Collections)/src/Transaction.java
@@ -0,0 +1,74 @@
+import java.util.Date;
+
+/**
+ * Stores information related to the renting and returning a book operations.
+ * @author george
+ *
+ */
+public class Transaction {
+	
+	/* Fields */
+	// The book for which the transaction is being performed.
+	private Book book;
+	// The date of requesting the current transaction.
+	private Date requestedDate;
+	// The date of serving of the current transaction.
+	private Date servedDate;
+	// The user id of the library member requesting or return the book.
+	private int userID;
+	// A global counter that keeps the counter (auto increment).
+	private static int transactionsCounter;
+	// A unique identifier that takes the current count value.
+	private final int TRANSACTION_ID;
+	
+	/* Constructors */
+	public Transaction() {
+		this.transactionsCounter++;
+		this.TRANSACTION_ID = transactionsCounter;
+	}
+	
+	public Transaction(Book book, Date requestedDate, Date servedDate, int userID) {
+		this();
+		this.book = book;
+		this.requestedDate = requestedDate;
+		this.servedDate = servedDate;
+		this.userID = userID;
+	}
+	
+	/* Setters & Getters */
+	public void setBook(Book book) {
+		this.book = book;
+	}
+	
+	public Book getBook() {
+		return this.book;
+	}
+	
+	public void setRequestedDate(Date date) {
+		this.requestedDate = date;
+	}
+	
+	public Date getRequestedDate() {
+		return this.requestedDate;
+	}
+	
+	public void setServedDate(Date date) {
+		this.servedDate = date;
+	}
+	
+	public Date getServedDate() {
+		return this.servedDate;
+	}
+	
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	
+	public int getUserID() {
+		return this.userID;
+	}
+	
+	public void setTransactionsCounter(int transactionsCounter) {
+		transactionsCounter = transactionsCounter;
+	}
+}
