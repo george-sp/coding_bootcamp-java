2f023dba88274ce3dcdae481495d72f87894d51f
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-22 12:36:31 +0300

    Exercise 2a - Create the Shape class that implements Agreeable interface.

---
+++ java_tutorial/interfaces/Exercise2/src/Agreeable.java
@@ -0,0 +1,5 @@
+
+public interface Agreeable {
+
+	boolean isSmallerThan(Agreeable other);
+}
---
+++ java_tutorial/interfaces/Exercise2/src/Shape.java
@@ -0,0 +1,19 @@
+
+public class Shape implements Agreeable{
+
+	private int width;
+	private int height;
+	
+	public Shape() {}
+	
+	public Shape(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+	
+	@Override
+	public boolean isSmallerThan(Agreeable other) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
