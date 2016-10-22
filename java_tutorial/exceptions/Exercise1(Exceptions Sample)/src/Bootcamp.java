b23b5d1b8f67de50b1c9053d90cf4ee56e9d3903
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-22 15:25:26 +0300

    Complete Exercise1 - exceptions.

--- java_tutorial/exceptions/Exercise1(Exceptions Sample)/src/Bootcamp.java
+++ java_tutorial/exceptions/Exercise1(Exceptions Sample)/src/Bootcamp.java
@@ -4,10 +4,12 @@ public class Bootcamp {
 	public static void main(String[] main) {
 		System.out.println(bye(26));
 		System.out.println(bye(24));
+		System.out.println(bye(25));
 	}
 	
 	public static int bye(double number) {
 		try {
+			System.out.println("\n" + number + "\n----");
 			welcome(number);
 		} catch (DigitException de) {
 			System.out.println("DigitException: " + de.getDigitExceptionMessage());
@@ -15,6 +17,8 @@ public class Bootcamp {
 		} catch (AnotherDigitException ade) {
 			System.out.println("AnotherDigitException: " + ade.getDigitExceptionMessage());
 			return 230;
+		} finally {
+			System.out.println("FINALLY ALWAYS GET EXECUTED!!");
 		}
 		return 0;
 	}
@@ -22,7 +26,7 @@ public class Bootcamp {
 	public static void welcome(double doubleArgument) throws DigitException, AnotherDigitException {
 		if (doubleArgument > 25) {
 			throw new DigitException();
-		} else {
+		} else if (doubleArgument < 25){
 			throw new AnotherDigitException();
 		}
 	}
