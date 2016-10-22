f8a509115c40aec90d8199db86340feea34d3dd6
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-22 15:18:21 +0300

    Exercise 1b - Catch these Exceptions.

--- java_tutorial/exceptions/Exercise1(Exceptions Sample)/src/Bootcamp.java
+++ java_tutorial/exceptions/Exercise1(Exceptions Sample)/src/Bootcamp.java
@@ -1,7 +1,25 @@
 
 public class Bootcamp {
+
+	public static void main(String[] main) {
+		System.out.println(bye(26));
+		System.out.println(bye(24));
+	}
 	
-	public static void welcome(double doubleArgument) throws DigitException, AnotherDigitException{
+	public static int bye(double number) {
+		try {
+			welcome(number);
+		} catch (DigitException de) {
+			System.out.println("DigitException: " + de.getDigitExceptionMessage());
+			return 605;
+		} catch (AnotherDigitException ade) {
+			System.out.println("AnotherDigitException: " + ade.getDigitExceptionMessage());
+			return 230;
+		}
+		return 0;
+	}
+
+	public static void welcome(double doubleArgument) throws DigitException, AnotherDigitException {
 		if (doubleArgument > 25) {
 			throw new DigitException();
 		} else {
