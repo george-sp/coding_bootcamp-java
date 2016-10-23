06d96208911835dd2cbe60f875aedc64fcd050a3
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-23 22:09:07 +0300

    Provide the Exercise's code skeleton.

---
+++ tdd_&_oop/03.recapitulation/Exercise/BootCampAdministration.java
@@ -0,0 +1,37 @@
+
+
+import java.util.ArrayList;
+
+public class BootCampAdministration {
+
+	//Create a private ArrayList with object of BootCampCandidates with name anArrayListOfBootCamps
+
+	
+	//Create a constructor which receives an array with BootCampCandidate objects and 
+	//initializes the anArrayListOfBootCamps ArrayList by adding all BootCampCandidate array's
+	//elements in the the nArrayListOfBootCamps
+
+	
+	//Create setters and getters for BootCampAdministration
+
+	
+
+	//create an int method that returns the size of anArrayListOfBootCamps
+
+	
+	//Create a method that adds a new BootCampCandidate inside the ArrayList
+
+	
+	//Create a method that removes a candidate by name and returns true if found or false if not
+	//HINT: you have to use get() to get the object and equals to compare it
+
+	
+	//Create a method to search for a boot camp member by surname and returns a string with its information
+	// if it's found and null if it's not found
+
+	
+	
+	//Create a method that can change a bootcampers age by having as input its name and surname
+
+	
+}
---
+++ tdd_&_oop/03.recapitulation/Exercise/BootCampCandidates.java
@@ -0,0 +1,17 @@
+
+
+public class BootCampCandidates {
+	
+	//Create three private fields a name a surname and age
+	
+	
+	//Create a constructor that receives three arguments and initializes the variables
+
+	
+	//Create getters and setters for all your variables (six in total methods needed)
+
+	
+	//Create a method toString that returns the name surname and age of the candidate
+
+
+}
