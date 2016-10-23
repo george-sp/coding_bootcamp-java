0a6c6a0c1e8564244ddba12e5949265f5c6cee47
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-24 00:22:24 +0300

    Test upcasting and downcasting.

--- tdd_&_oop/05.inheritance/Example(Polymorphism)/src/polym/TesterPolym.java
+++ tdd_&_oop/05.inheritance/Example(Polymorphism)/src/polym/TesterPolym.java
@@ -9,17 +9,34 @@ public class TesterPolym {
 
 		System.out.println(animal1);
 		System.out.println(animal2);
-//		animal1.getLegs(); illegal!!
-		
-		//create Dogs
-		Animal dog1 = new Dog();//default constructor
-		Animal dog2 = new Dog("Lacy",4);//overloaded constructor
-		
+		// animal1.getLegs(); ILLEGAL!!
+
+		// create Dogs
+		Animal dog1 = new Dog();// default constructor
+		Animal dog2 = new Dog("Lacy", 4);// overloaded constructor
+
 		System.out.println(dog1);
-		System.out.println(dog2);		
-		System.out.println(dog2.getTail()); //inherited method
-		dog2.move();//calling the overridden method
-//		dog1.dogOnlyMethod(); illegal!!
+		System.out.println(dog2);
+		System.out.println(dog2.getTail()); // inherited method
+		dog2.move();// calling the overridden method
+		// dog1.dogOnlyMethod(); ILLEGAL!!
+		// dog1.getLegs(); ILLEGAL!!
+
+		/**
+		 * When reference variable of Parent class refers to the object of Child
+		 * class, it is known as upcasting.
+		 **/
+		Animal animal3 = new Dog("Rex", 4);
+		animal3.move();// calling subclass overridden move
+		// animal3.getLegs(); ILLEGAL!!
+
+		/*
+		 * When Subclass type refers to the object of Parent class, it is known
+		 * as downcasting.
+		 */
+		Dog dog3 = (Dog) animal3;
+		System.out.println(dog3.getLegs());
+		// System.out.println(((Dog)animal3).getLegs()); // lines 36 & 37 in one
 	}
 
 }
