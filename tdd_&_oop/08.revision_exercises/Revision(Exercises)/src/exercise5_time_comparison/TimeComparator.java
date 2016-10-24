8982a32e27361fdc81a00c6848b5480ced2e2c0c
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-24 13:58:03 +0300

    Create the TimeComparator class in exercise5.

--- tdd_&_oop/08.revision_exercises/Revision(Exercises)/src/exercise5_time_comparison/TimeComparator.java
+++ tdd_&_oop/08.revision_exercises/Revision(Exercises)/src/exercise5_time_comparison/TimeComparator.java
@@ -1,5 +1,27 @@
 package exercise5_time_comparison;
 
-public class TimeComparator {
+import java.util.Comparator;
+
+public class TimeComparator implements Comparator<TimeCom>{
+	
+	@Override
+	public int compare(TimeCom time1, TimeCom time2) {
+		int timeDifference = time1.getHour() - time2.getHour();
+		if (timeDifference == 0) {
+			int minuteDifference = time1.getMinute() - time2.getMinute();
+			if (minuteDifference == 0) {
+				int secondDifference = time1.getSecond() - time2.getSecond();
+				if (secondDifference == 0) {
+					return 0;
+				} else {
+					return secondDifference;
+				}
+			} else {
+				return minuteDifference;
+			}
+		} else {
+			return timeDifference;
+		}
+	}
 
 }
