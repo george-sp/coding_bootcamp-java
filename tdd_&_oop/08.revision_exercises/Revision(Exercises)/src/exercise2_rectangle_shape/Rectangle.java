7c7bbfbf162cbeb1d17f916a7e2a9b949500b4de
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-24 12:19:33 +0300

    Provide Rectangle with extra functionalities.

--- tdd_&_oop/08.revision_exercises/Revision(Exercises)/src/exercise2_rectangle_shape/Rectangle.java
+++ tdd_&_oop/08.revision_exercises/Revision(Exercises)/src/exercise2_rectangle_shape/Rectangle.java
@@ -1,34 +1,70 @@
 package exercise2_rectangle_shape;
 
 public class Rectangle {
-	
+
 	int sideA;
 	int sideB;
-	
-	public Rectangle() {}
-	
+
+	public Rectangle() {
+	}
+
 	public Rectangle(int a, int b) {
 		this.sideA = a;
 		this.sideB = b;
 	}
-	
+
 	public void setSideA(int a) {
 		this.sideA = a;
 	}
-	
+
 	public int getSideA() {
 		return this.sideA;
 	}
-	
+
 	public void setSideB(int b) {
 		this.sideB = b;
 	}
-	
+
 	public int getSideB() {
 		return this.sideB;
 	}
-	
+
 	public String toString() {
 		return "Rectangle\n\tsideA: " + sideA + "\n\tsideB: " + sideB;
+	}
+
+	public boolean areEqual(Rectangle r) {
+		return this.sideA * this.sideB == r.sideA * r.sideB;
+	}
+
+	public boolean isSquare() {
+		return this.sideA == this.sideB;
+	}
+
+	public String draw() {
+		String str = "";
+		for (int i = 0; i <= this.sideA; i++) {
+			if (i == 0 || i == sideA) {
+				for (int j = 0; j < this.sideB; j++) {
+					str += "-";
+				}
+				str += "\n";
+			}
+			if (i < this.sideA) {
+				for (int j = 0; j < this.sideB; j++) {
+					if (j == 0 || j == this.sideA - 1) {
+						str += "| ";
+					} else {
+						str += " ";
+					}
+				}
+				str += "\n";
+			}
+		}
+		return str;
+	}
+
+	public Rectangle copy() {
+		return this;
 	}
 }
--- tdd_&_oop/08.revision_exercises/Revision(Exercises)/src/exercise2_rectangle_shape/TesterRectangle.java
+++ tdd_&_oop/08.revision_exercises/Revision(Exercises)/src/exercise2_rectangle_shape/TesterRectangle.java
@@ -5,8 +5,19 @@ public class TesterRectangle {
 	public static void main(String[] args) {
 		Rectangle r1 = new Rectangle();
 		Rectangle r2 = new Rectangle(2,3);
+		Rectangle r3 = new Rectangle(3,2);
+		Rectangle r4 = new Rectangle(4,4);
 		
 		System.out.println(r1.toString());
 		System.out.println(r2.toString());
+		System.out.println("r1.areEqual(r2) " + r1.areEqual(r2));
+		System.out.println("r2.areEqual(r3) " + r2.areEqual(r3));
+		System.out.println("r3.isSquare() " + r3.isSquare());
+		System.out.println("r4.isSquare() " + r4.isSquare());
+		Rectangle r5 = r4.copy();
+//		r5.setSideA(5);
+		System.out.println(r4.toString());
+		System.out.println(r5.toString());
+		System.out.println(r3.draw());
 	}
 }
