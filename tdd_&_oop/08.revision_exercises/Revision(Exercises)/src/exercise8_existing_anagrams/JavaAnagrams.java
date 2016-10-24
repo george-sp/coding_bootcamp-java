edccd02b2c51067bfd1bc975d4a5de7b7a75c3f4
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-24 17:18:18 +0300

    Find all permutations of a String.

--- tdd_&_oop/08.revision_exercises/Revision(Exercises)/src/exercise8_existing_anagrams/JavaAnagrams.java
+++ tdd_&_oop/08.revision_exercises/Revision(Exercises)/src/exercise8_existing_anagrams/JavaAnagrams.java
@@ -5,22 +5,37 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class JavaAnagrams {
 
 	public static void main(String[] args) {
+		permutation("", "anagram");
+		
+		ArrayList<String> dictionaryArrayList = new ArrayList<>();
 		Charset charset = Charset.forName("US-ASCII");
 		try (BufferedReader reader = Files.newBufferedReader(Paths.get("./src/exercise8_existing_anagrams/dictionary.txt"), charset)) {
 		    String line = null;
 		    while ((line = reader.readLine()) != null) {
-		        System.out.println(line);
+//		        System.out.println(line + " length: " + line.length());
+		    	dictionaryArrayList.add(line);
 		    }
 		} catch (IOException x) {
 		    System.err.format("IOException: %s%n", x);
 		}
 	}
 
+	private static void permutation(String temp, String word) {
+	    int n = word.length();
+	    if (n == 0) System.out.println(temp);
+	    else {
+	        for (int i = 0; i < n; i++)
+	            permutation(temp + word.charAt(i), 
+	            		word.substring(0, i) + word.substring(i+1, n));
+	    }
+	}
+	
 	public static boolean isAnagram(String a, String b) {
 		char[] charArrayA = a.toLowerCase().toCharArray();
 		char[] charArrayB = b.toLowerCase().toCharArray();
