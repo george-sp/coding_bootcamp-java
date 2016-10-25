92b00b38ec196271c4057d0a3869626045903577
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-26 00:07:13 +0300

    Complete Exercise8: Existing Anagrams.

--- tdd_&_oop/08.revision_exercises/Revision(Exercises)/src/exercise8_existing_anagrams/JavaAnagrams.java
+++ tdd_&_oop/08.revision_exercises/Revision(Exercises)/src/exercise8_existing_anagrams/JavaAnagrams.java
@@ -12,35 +12,42 @@ import java.util.Scanner;
 
 public class JavaAnagrams {
 
+	// Stores all the permutations (anagrams) of the given word.
 	static ArrayList<String> permutationsArrayList = new ArrayList<>();
+	// Stores each valid(in dictionary) anagram only once.
 	static HashSet<String> anagramsInDictionary = new HashSet<>();
+	// Stores the words in the dictionary.txt file.
+	static ArrayList<String> dictionaryArrayList = new ArrayList<>();
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-
-		ArrayList<String> dictionaryArrayList = new ArrayList<>();
 		
+		// Reading the dictionary file.
 		Charset charset = Charset.forName("US-ASCII");
 		try (BufferedReader reader = Files.newBufferedReader(Paths.get("./src/exercise8_existing_anagrams/dictionary.txt"), charset)) {
 		    String line = null;
 		    while ((line = reader.readLine()) != null) {
-//		        System.out.println(line + " length: " + line.length());
 		    	dictionaryArrayList.add(line);
 		    }
 		} catch (IOException x) {
 		    System.err.format("IOException: %s%n", x);
 		}
 		
+		// Prompt user to give a word.
 		System.out.println("Enter a word: ");
 		String input = scanner.nextLine();
+		
+		// Get all permutations of the given word.
 		permutation("", input);
 		
+		// Get the each valid anagram only once.
 		for (String permutation : permutationsArrayList) {
 			if (dictionaryArrayList.contains(permutation)){
 				anagramsInDictionary.add(permutation);				
 			}
 		}
 		
+		// Print them.
 		for (String anagramInDic : anagramsInDictionary) {
 			System.out.println(anagramInDic);
 		}
@@ -55,7 +62,6 @@ public class JavaAnagrams {
 	private static void permutation(String temp, String word) {
 	    int n = word.length();
 	    if (n == 0) {
-//	    	System.out.println(temp);
 	    	permutationsArrayList.add(temp);
 	    }
 	    else {
