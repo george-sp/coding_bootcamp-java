c0af1b1e3ac4b61498505a1b7524cea77dc2b594
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-25 22:15:18 +0300

    Check if every anagram exists in dictionary.

--- tdd_&_oop/08.revision_exercises/Revision(Exercises)/src/exercise8_existing_anagrams/JavaAnagrams.java
+++ tdd_&_oop/08.revision_exercises/Revision(Exercises)/src/exercise8_existing_anagrams/JavaAnagrams.java
@@ -7,13 +7,17 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class JavaAnagrams {
 
+	static ArrayList<String> permutationsArrayList = new ArrayList<>();
+	
 	public static void main(String[] args) {
-		permutation("", "anagram");
-		
+		Scanner scanner = new Scanner(System.in);
+
 		ArrayList<String> dictionaryArrayList = new ArrayList<>();
+		
 		Charset charset = Charset.forName("US-ASCII");
 		try (BufferedReader reader = Files.newBufferedReader(Paths.get("./src/exercise8_existing_anagrams/dictionary.txt"), charset)) {
 		    String line = null;
@@ -24,11 +28,30 @@ public class JavaAnagrams {
 		} catch (IOException x) {
 		    System.err.format("IOException: %s%n", x);
 		}
+		
+		System.out.println("Enter a word: ");
+		String input = scanner.nextLine();
+		permutation("", input);
+		
+		for (String permutation : permutationsArrayList) {
+			if (dictionaryArrayList.contains(permutation)){
+				System.out.println(permutation);				
+			}
+		}
+		
+		try {
+			scanner.close();
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		}
 	}
 
 	private static void permutation(String temp, String word) {
 	    int n = word.length();
-	    if (n == 0) System.out.println(temp);
+	    if (n == 0) {
+//	    	System.out.println(temp);
+	    	permutationsArrayList.add(temp);
+	    }
 	    else {
 	        for (int i = 0; i < n; i++)
 	            permutation(temp + word.charAt(i), 
