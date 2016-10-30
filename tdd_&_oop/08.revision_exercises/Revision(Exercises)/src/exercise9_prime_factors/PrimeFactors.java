9cca7545bab1b7f87234b19d13bb18a09a1b6275
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-30 12:00:22 +0200

    Complete Exercise9: PrimeFactors (for BigIntegers).

--- tdd_&_oop/08.revision_exercises/Revision(Exercises)/src/exercise9_prime_factors/PrimeFactors.java
+++ tdd_&_oop/08.revision_exercises/Revision(Exercises)/src/exercise9_prime_factors/PrimeFactors.java
@@ -1,24 +1,28 @@
 package exercise9_prime_factors;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.List;
 
 public class PrimeFactors {
 
 	public static void main(String[] args) {
-		System.out.println(getPrimeFactors(13195).get(getPrimeFactors(13195).size() - 1));
+//		System.out.println(getPrimeFactors(13195).get(getPrimeFactors(13195).size() - 1));
+		BigInteger b1 = new BigInteger("600851475143");
+		List<BigInteger> primeFactors = getPrimeFactors(b1);
+		System.out.println("The largest prime factor of " + b1.toString() + " is: " + primeFactors.get(primeFactors.size() - 1));
 	}
 
-	public static ArrayList<Integer> getPrimeFactors(int numbers) {
-		int n = numbers;
-		ArrayList<Integer> primeFactors = new ArrayList<Integer>();
-		for (int i = 2; i <= n / i; i++) {
-			while (n % i == 0) {
+	public static List<BigInteger> getPrimeFactors(BigInteger number) {
+		List<BigInteger> primeFactors = new ArrayList<BigInteger>();
+		for (BigInteger i = new BigInteger("2"); i.compareTo(number.divide(i)) <= 0 ; i = i.add(BigInteger.ONE)) {
+			while (number.mod(i) == BigInteger.ZERO) {
 				primeFactors.add(i);
-				n /= i;
+				number = number.divide(i);
 			}
 		}
-		if (n > 1) {
-			primeFactors.add(n);
+		if (number.compareTo(BigInteger.ONE) == 1) {
+			primeFactors.add(number);
 		}
 		return primeFactors;
 	}
