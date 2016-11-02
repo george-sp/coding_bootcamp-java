f8ab7f8a31ebab34b6916aeff02ac74062879d1a
George Spiridakis <spiridakis.grg@gmail.com>
2016-11-02 14:51:11 +0200

    Extend the Hello Servlet.

--- web_development/00.servlets/bootcamp/src/org/afdemp/bootcamp/george/HelloServlet.java
+++ web_development/00.servlets/bootcamp/src/org/afdemp/bootcamp/george/HelloServlet.java
@@ -27,7 +27,8 @@ public class HelloServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Hello, World!");
+		String name = request.getParameter("name");
+		response.getWriter().append("Hello, ").append(name).append("!");
 	}
 
 	/**
