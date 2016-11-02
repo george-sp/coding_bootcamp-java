5e7c8ad4a2d1800a340c3f307cc1b7fa0117aff8
George Spiridakis <spiridakis.grg@gmail.com>
2016-11-02 21:46:10 +0200

    Complete Exercise5 in servlets.

---
+++ web_development/00.servlets/Exercise5/src/com/exercise5/george/GameServlet.java
@@ -0,0 +1,41 @@
+package com.exercise5.george;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GameServlet
+ */
+@WebServlet("/play")
+public class GameServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Game mGame;
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GameServlet() {
+        super();
+        this.mGame = new Game();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append(this.mGame.play());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
