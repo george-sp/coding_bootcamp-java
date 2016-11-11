7911844f8d3ae78e006051d79834abcd67d88a2f
George Spiridakis <spiridakis.grg@gmail.com>
2016-11-11 11:15:05 +0200

    Generate javadoc for HelloServlet.

--- maven/hello_servlet/src/main/java/servlets/HelloServlet.java
+++ maven/hello_servlet/src/main/java/servlets/HelloServlet.java
@@ -6,8 +6,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * A simple Servlet that handles a GET request.
+ *
+ * @author George Spyridakis
+ */
 public class HelloServlet extends HttpServlet {
 
+    /**
+     * Handles GET requests.
+     *
+     * @param request the HTTP request.
+     * @param response the HTTP resopnse.
+     * @throws IOException in case an I/O occurs
+     */
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
