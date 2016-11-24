4b8cc0239ded786ea9a5151fd67cde8a9a4c92c9
George Spiridakis <spiridakis.grg@gmail.com>
2016-11-24 13:03:04 +0200

    Complete Syntax: Control flow (if statement) exercise.

--- front_end/02.javascript/exercises.js
+++ front_end/02.javascript/exercises.js
@@ -1,3 +1,21 @@
 // Syntax: Variables
 // Create a variable with name greeting and value Hello world!
 var greeting = 'Hello world!';
+
+// Syntax: Control flow (if statements)
+/*
+You are provided with two variables (you don’t have to declare them):
+- A number: n (you can control it’s value using the input below)
+- A boolean: isZero
+
+If the value of n equals 0, then assign the value true to isZero.
+If the value of n does not equal 0, then assign the value false to isZero.
+*/
+var n = 0;
+var isZero;
+
+if (n === 0) {
+  isZero = true;
+} else if (n !== 0) {
+  isZero = false;
+}
