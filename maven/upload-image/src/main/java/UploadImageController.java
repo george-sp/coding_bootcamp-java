6e0ce4ee4030a2fd4c2d6eb10cc9c25848281c29
George Spiridakis <spiridakis.grg@gmail.com>
2016-12-01 22:13:43 +0200

    Upload an image file.

---
+++ maven/upload-image/src/main/java/UploadImageController.java
@@ -0,0 +1,110 @@
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+/**
+ * Servlet implementation class UploadImageController
+ */
+@WebServlet("/upload")
+@MultipartConfig(maxFileSize = 16177215)
+public class UploadImageController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	// Parameter names
+	private static final String PROFILE_IMAGE = "image";
+	private static final String NAME = "name";
+	// The upload location
+	private static final String UPLOAD_LOCATION = "uploaded_images";
+	// Dispatcher for the index page
+	RequestDispatcher indexDispatcher;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public UploadImageController() {
+		super();
+	}
+
+	@Override
+	public void init() {
+		indexDispatcher = getServletContext().getRequestDispatcher("/index.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().append("Served: ").append("name: ").append(request.getParameter("name\n"));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
+
+		String name = request.getParameter(NAME);
+		// Obtain the upload file part in this multipart request
+		Part imagePart = request.getPart(PROFILE_IMAGE);
+		// Get the content type
+		String contentType = imagePart.getContentType();
+
+		String message = null;
+		String status = null;
+
+		if (imagePart.getSize() != 0) {
+			// Check if the selected file is not an image
+			if (!contentType.split("/")[0].equals("image")) {
+				status = "error";
+				message = "Error in uploading an image file!";
+				request.setAttribute("status", status);
+				request.setAttribute("message", message);
+				indexDispatcher.forward(request, response);
+				return;
+			}
+
+			// Get the absolute path of the web application
+			String appPath = request.getServletContext().getRealPath("");
+			// Construct path of the directory to save uploaded files
+			String savePath = appPath + File.separator + UPLOAD_LOCATION;
+			// Define the path to the final storage location
+			File uploadFile = new File(savePath);
+			if (!uploadFile.exists()) {
+				uploadFile.mkdir();
+			}
+			contentType = "." + imagePart.getContentType().split("/")[1];
+			String uploadFileName = name + contentType;
+			File file = new File(uploadFile, uploadFileName);
+
+			try (InputStream input = imagePart.getInputStream()) {
+				Files.copy(input, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+				status = "success";
+				message = "<b>Image Uploaded Successfully!</b>" + "<br>name: " + name + "<br>contentType: " + contentType
+						+ "<br>image size: " + imagePart.getSize() + "<br>appPath: " + appPath + "<br>savePath: " + savePath
+						+ "<br>uploadFileName: " + uploadFileName;
+				request.setAttribute("status", status);
+				request.setAttribute("message", message);
+				indexDispatcher.forward(request, response);
+				return;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
--- maven/upload-image/src/main/webapp/index.jsp
+++ maven/upload-image/src/main/webapp/index.jsp
@@ -2,9 +2,10 @@
 	pageEncoding="UTF-8"%>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Upload Image</title>
 <link rel="stylesheet"
 	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
@@ -16,21 +17,42 @@
 	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
 	integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
 	crossorigin="anonymous">
+
+<!-- Custom theme -->
+<link rel='stylesheet' href='style.css' type='text/css' media='all' />
 </head>
+<%
+	String message = (String) request.getAttribute("message");
+	String status = (String) request.getAttribute("status");
+%>
 <body>
-	<br>
-	<div class="col-sm-4 col-sm-offset-4 panel panel-default">
-		<div class="panel-body">
-			<form class="form-signin" method="POST" action="login_simple"
-				enctype="multipart/form-data" class="form-signup">
-				<h2 class="form-signin-heading login-or-signup text-center">Upload
-					your image</h2>
-				<input type="text" class="form-control"
-					placeholder="enter your name" name="name" required autofocus>
-				<input type="file" name="image" class="form-control"> <br>
-				<button class="btn btn-lg btn-primary btn-block" type="submit">Upload</button>
-			</form>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4 col-sm-offset-4 panel panel-default">
+				<div class="panel-body">
+					<form method="POST" action="upload" enctype="multipart/form-data"
+						class="form-signup">
+						<h2 class="form-signin-heading login-or-signup text-center">Upload
+							your image</h2>
+						<input type="text" class="form-control"
+							placeholder="enter your name" name="name" required autofocus>
+						<input type="file" name="image" class="form-control"> <br>
+						<button class="btn btn-lg btn-primary btn-block" type="submit">Upload</button>
+					</form>
+				</div>
+			</div>
 		</div>
+		<%
+			if (null != message) {
+		%>
+		<div class="row">
+			<div class="col-sm-6 col-sm-offset-3">
+				<%="<div class='" + status + "'><p>" + message + "</p></div>"%>
+			</div>
+		</div>
+		<%
+			}
+		%>
 	</div>
 </body>
 </html>
