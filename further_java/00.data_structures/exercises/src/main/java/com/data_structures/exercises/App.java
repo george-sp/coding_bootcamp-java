b8e2d3253dad1cb280183bc1581eeb9e68a253e7
George Spiridakis <spiridakis.grg@gmail.com>
2016-12-04 03:51:00 +0200

    Complete Exercise3 - intersection on HahMaps.

--- further_java/00.data_structures/exercises/src/main/java/com/data_structures/exercises/App.java
+++ further_java/00.data_structures/exercises/src/main/java/com/data_structures/exercises/App.java
@@ -3,14 +3,17 @@ package com.data_structures.exercises;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 import com.data_structures.exercises.exercise1.ListUtils;
 import com.data_structures.exercises.exercise2.IntegerComparator;
+import com.data_structures.exercises.exercise3.HashMapUtils;
 
 /**
  * Hello world!
@@ -25,6 +28,7 @@ public class App {
 
 		System.out.println("listOne: " + listOne + "\nlistTwo: " + listTwo + "\nintersectionList: "
 				+ ListUtils.intersection(listOne, listTwo) + "\nunionList: " + ListUtils.union(listOne, listTwo));
+
 		/* Exercise 2 - PriorityQueue */
 		System.out.println("Exercise2 starts!");
 		// Define an IntegerComparator and a Priority Queue
@@ -41,5 +45,27 @@ public class App {
 		while (integerIterator.hasNext()) {
 			System.out.println("remove from PriorityQueue: " + queue.remove());
 		}
+
+		/* Exercise 3 - HashMap intersection */
+		System.out.println("Exercise3 starts!");
+		HashMap<String, Double> hashMapOne = new HashMap<String, Double>();
+		hashMapOne.put("one", 1.0);
+		hashMapOne.put("two", 2.0);
+		// hashMapOne.put(null, 2.5);
+		hashMapOne.put("three", 3.0);
+		hashMapOne.put("six", null);
+		hashMapOne.put("four", 4.0);
+		hashMapOne.put("seven", null);
+		System.out.println("hashMapOne: " + hashMapOne);
+		HashMap<String, Double> hashMapTwo = new HashMap<String, Double>();
+		hashMapTwo.put("two", 2.02);
+		hashMapTwo.put(null, 2.5);
+		hashMapTwo.put("three", 3.03);
+		hashMapTwo.put("six", 6.0);
+		hashMapTwo.put("four", 4.04);
+		hashMapTwo.put("five", 5.05);
+		hashMapTwo.put("seven", null);
+		System.out.println("hashMapTwo: " + hashMapTwo);
+		System.out.println("intersection: " + HashMapUtils.intersection(hashMapOne, hashMapTwo));
 	}
 }
