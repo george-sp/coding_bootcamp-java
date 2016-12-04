a5ff1f5f554d1326cd66e1d14c56b441c36a7a39
George Spiridakis <spiridakis.grg@gmail.com>
2016-12-04 20:44:04 +0200

    Complete Exercise1 - counter loop.

--- further_java/01.threads/exercises/src/main/java/com/further_java/threads/exercises/App.java
+++ further_java/01.threads/exercises/src/main/java/com/further_java/threads/exercises/App.java
@@ -1,13 +1,34 @@
 package com.further_java.threads.exercises;
 
+import com.further_java.threads.exercises.exercise1.CounterLoop;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		int patience = Integer.parseInt(args[0]);
+
+		Thread counterLoop = new CounterLoop(Integer.parseInt(args[1]));
+		counterLoop.start();
+
+		long startTime = System.currentTimeMillis();
+
+		try {
+			while (counterLoop.isAlive()) {
+				counterLoop.join(1000);
+				long currentTime = System.currentTimeMillis() - startTime;
+				System.out.println("currentTime: " + currentTime + ", patience: " + patience);
+
+				if (currentTime > patience) {
+					System.out.println(Thread.currentThread().getName() + ": interrupts the CounterLoop");
+					counterLoop.interrupt();
+					counterLoop.join();
+				}
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 }
