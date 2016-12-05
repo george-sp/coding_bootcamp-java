c276a073a16948aeaf2d71a211517111c63ff4b8
George Spiridakis <spiridakis.grg@gmail.com>
2016-12-05 13:14:57 +0200

    Start exercises in design_patterns.

---
+++ 00.design_patterns/design_patterns/src/main/java/com/advanced_oop/exercises/design_patterns/App.java
@@ -0,0 +1,13 @@
+package com.advanced_oop.exercises.design_patterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
