5daa58ef434d9dd9f5db06257ad1e8ff85fd568c
George Spiridakis <spiridakis.grg@gmail.com>
2016-12-13 20:03:10 +0200

    Start 'hello-secure' project.

---
+++ spring/hello-secure/src/main/java/com/example/HelloSecureApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSecureApplication.class, args);
+	}
+}
